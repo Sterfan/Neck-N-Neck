620fca55ec80ee3490c6b44ac470e38bd4901af6
Joss <joseph@delatolas.se>
2019-04-10 16:43:48 +0200

    Neck script working PERFECTLY
    Probably.. We just need a neck sprite with the correct size

---
+++ GiraffeS/Assets/Neck.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Neck : MonoBehaviour
+{
+    public GameObject startPoint;
+    public GameObject endPoint;
+    public Vector3 startPosition;
+    public Vector3 endPosition;
+    
+    private void Update()
+    {
+        startPosition = startPoint.transform.position;
+        endPosition = endPoint.transform.position;
+        Strech(gameObject, startPosition, endPosition);
+    }
+
+    public void Strech(GameObject _sprite, Vector3 _initialPosition, Vector3 _finalPosition)
+    {
+        float width = _sprite.GetComponent<SpriteRenderer>().bounds.size.x;
+        Vector3 centerPos = (_initialPosition + _finalPosition) / 2f;
+        _sprite.transform.position = centerPos;
+        Vector3 direction = _finalPosition - _initialPosition;
+        direction = Vector3.Normalize(direction);
+        _sprite.transform.right = direction;
+        Vector3 scale = new Vector3(1, 1, 1);
+        scale.x = Vector3.Distance(_initialPosition, _finalPosition) / width;
+        _sprite.transform.localScale = scale;
+    }
+}
