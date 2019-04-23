2e54df43fa710b88fed09e358b4675d053f1003a
Joss <joseph@delatolas.se>
2019-04-23 13:39:45 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ GiraffeS/Assets/Scripts/Utility/FinishLine.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class FinishLine : MonoBehaviour
+{
+    GameObject backgroundScrl;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.CompareTag("Giraffe"))
+        {
+            backgroundScrl = collision.gameObject;
+            Debug.Log("We all up in this bitch");
+            gameObject.GetComponentInParent<BackgroundScroller>().enabled = false;
+        }
+    }
+}
