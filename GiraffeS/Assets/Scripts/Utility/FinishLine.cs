aa4044b07e7f222d7047594309ca021b394dab9f
Joss <joseph@delatolas.se>
2019-04-26 12:40:44 +0200

    After the finish line the giraffes run.

--- GiraffeS/Assets/Scripts/Utility/FinishLine.cs
+++ GiraffeS/Assets/Scripts/Utility/FinishLine.cs
@@ -4,6 +4,10 @@ public class FinishLine : MonoBehaviour
 {
     GameObject backgroundScrl;
     public GameObject progressTracker;
+    public GameObject giraffe;
+    bool endOfRace = false;
+    Vector2 newPosition;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.transform.CompareTag("Giraffe"))
@@ -12,6 +16,22 @@ public class FinishLine : MonoBehaviour
             Debug.Log("We all up in this bitch");
             gameObject.GetComponentInParent<BackgroundScroller>().enabled = false;
             progressTracker.GetComponent<PlayerProgress>().isFinished = true;
+            endOfRace = true;
+        }
+    }
+
+    void Start()
+    {
+        newPosition = giraffe.GetComponent<Rigidbody2D>().position;
+    }
+
+    void FixedUpdate()
+    {
+        if (endOfRace)
+        {
+            Debug.Log("desosdfalk?");
+            newPosition = newPosition + new Vector2(0.5f, 0);
+            giraffe.GetComponent<Rigidbody2D>().MovePosition(newPosition);
         }
     }
 }
