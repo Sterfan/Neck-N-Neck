a5b399537adf498816d99f3a073f99fee7917a2d
Marina <marina.arknell@gmail.com>
2019-04-30 11:32:33 +0200

    Update savannah/marina

---
+++ GiraffeS/Assets/Scripts/BananaSpawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BananaSpawner : MonoBehaviour
+{
+    public GameObject Banana;
+    public Transform[] SpawnPoints;
+    void Start()
+    {
+        StartCoroutine(StartSpawning());
+    }
+
+    IEnumerator StartSpawning()
+    {
+        yield return new WaitForSeconds(Random.Range(1f, 3.5f));
+        Instantiate(Banana, SpawnPoints[Random.Range(0, SpawnPoints.Length)].position, Quaternion.identity);
+        StartCoroutine(StartSpawning());
+    }
+}
