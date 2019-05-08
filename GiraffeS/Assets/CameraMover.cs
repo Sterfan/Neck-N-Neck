c20b32251c755f062f006901213eaedab3fe64ed
Axel Thomsen <axelthomsen@hotmail.com>
2019-05-08 11:07:57 +0200

    just test stuff for background

---
+++ GiraffeS/Assets/CameraMover.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMover : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position += Vector3.right * Time.deltaTime * 30;
+        // transform.Translate(Vector2.right * Time.deltaTime * 30);
+
+    }
+}
