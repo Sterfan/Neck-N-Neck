cb0046e96ef2bc42abfd89776966f7ace440b098
Joss <joseph@delatolas.se>
2019-05-20 20:42:27 +0200

    Camera Smoothly Follows the ground level
    Additionally we can build the project now

---
+++ GiraffeS/Assets/Ghost.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ghost : MonoBehaviour
+{
+    public GameObject grff;
+    void Update()
+    {
+        //Debug.Log("E dis graff ie posshan " + grff.transform.position.x + " = " + transform.position.x);
+        transform.position = new Vector2(grff.transform.position.x, transform.position.y);
+        //Debug.Log(transform.position.x);
+    }
+}
