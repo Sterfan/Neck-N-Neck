b4d97e1250a9fd805c7f00c3593105e75da1a8c8
Joss <joseph@delatolas.se>
2019-05-24 16:41:53 +0200

    Fade in done and implemented on Axel's scene

---
+++ GiraffeS/Assets/FadeIn.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FadeIn : MonoBehaviour
+{
+    public Image backInBlack;
+
+    private void Start()
+    {
+        StartCoroutine("fadeIn");
+    }
+
+    IEnumerator fadeIn()
+    {
+        while (true)
+        {
+            backInBlack.color -= new Color(0f, 0f, 0f, 0.02f);
+            yield return null;
+            if (backInBlack.color.a <= 0f)
+            {
+                Destroy(this);
+            }
+        }
+    }
+}
