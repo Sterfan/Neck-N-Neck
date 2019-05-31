f3f6f0215a32a3b7a95c560d7bf67ba8fb7deca6
Joss <joseph@delatolas.se>
2019-05-31 14:48:26 +0200

    I am not a tool. I just work a lot when I'm hard

---
+++ GiraffeS/Assets/Scripts/ExitBufferSceneorsomeshtlikethat.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitBufferSceneorsomeshtlikethat : MonoBehaviour
+{
+    bool started;
+
+    public bool HasStarted { set { started = value; } }
+    void Update()
+    {
+        StartCoroutine("SuckMyAss");
+    }
+
+    IEnumerator SuckMyAss()
+    {
+        yield return new WaitForSeconds(10);
+        GetComponent<FadeOut>().StartCoroutine("fadeOut");
+    }
+
+}
